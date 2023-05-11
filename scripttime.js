b49417d93e98bff2baac9f066864df59463895ce
menmozhii <125111018+menmozhii@users.noreply.github.com>
2023-05-11 19:37:50 +0530

    Add files via upload

---
+++ scripttime.js
@@ -0,0 +1,56 @@
+var div=document.createElement("div");
+div.setAttribute("class","hell");
+div.style.textAlign="center";
+div.style.marginTop="150px";
+div.style.color="blue";
+document.body.append(div);
+
+
+var  hell = document.querySelector(".hell");
+setTimeout(function(){
+    hell.innerHTML="10";
+    setTimeout(function(){
+hell.innerHTML="9";
+setTimeout(function(){
+    hell.innerHTML="8";
+    setTimeout(function(){
+        hell.innerHTML="7";
+        setTimeout(function(){
+            hell.innerHTML="6";
+
+    setTimeout(function(){
+        hell.innerHTML="5";
+
+    
+    setTimeout(function(){
+        hell.innerHTML="4";
+        setTimeout(function(){
+            hell.innerHTML="3";
+            
+            setTimeout(function(){
+                hell.innerHTML="2";
+                setTimeout(function(){
+                    hell.innerHTML="1";
+
+                    setTimeout(function(){
+            hell.innerHTML="HAPPY NEW YEAR!!!";
+             
+        },2000)
+    },1000)
+
+
+        },1000)
+
+        },1000)
+
+    },1000)
+        },1000)
+
+    },1000)
+
+},1000)
+    },1000)
+
+},1000)
+},1000)
+
